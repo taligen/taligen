f10a3f11444a3cc43f8d95d56e8944d9d254b97b
Tammy Ernst <TammyKErnst@yahoo.com>
2017-09-06 12:06:50 +0000

    call understands paths and parameters. Other fixes too.

--- taligen.py
+++ taligen.py
@@ -7,6 +7,7 @@ import re
 import datetime
 import json
 import os
+import sys
 from collections import deque
 
 
@@ -16,14 +17,17 @@ def add_original_description(step, section, description):
     else:
         ss = step.get(section, {})
         d = ss.get("raw_description", "")
-        d += description
+        if d == "":
+            d = description
+        else:
+            d += "<br>" + description
         ss["raw_description"] = d
         ss["description"] = d
         step[section] = ss
     return step
 
 
-def add_comment(script, step, section, comment):
+def add_comment(step, section, comment):
     if section == "":
         c = step.get("comment", "")
         step["comment"] = c + comment
@@ -36,14 +40,27 @@ def add_comment(script, step, section, comment):
     return step
 
 
+def add_error(step, section, error):
+    if section == "":
+        c = step.get("error", "")
+        step["error"] = c + error
+    else:
+        ss = step.get(section, {})
+        c = ss.get("error", "")
+        c += error
+        ss["error"] = c
+        step[section] = ss
+    return step
+
+
 def add_step(steps, step, section):
-    if section != "" or "comment" in step or "call" in step:
+    if section != "" or "comment" in step or "call" in step or "set" in step or "error" in step:
         steps.append(step)
     return steps
 
 
 def arglist_to_paramdict(arglist):
-    if isinstance(arglist, basestring):
+    if isinstance(arglist, str):
         return arglist_to_paramdict(arglist.split(","))
     paramdict = {}
     for arg in arglist:
@@ -62,22 +79,76 @@ def params_to_string(substitutions):
     return paramstring
 
 
-def read_through_file(filename, script, parsed_scripts, filestack):
-    if filename in parsed_scripts:
-        # print("found already parsed " + filename)
-        return parsed_scripts[filename]
+def find_parameterized_file(path, filename, parameters, filestack):
+    (fdir, fname) = os.path.split(filename)
+    (fbase, fext) = os.path.splitext(fname)
+    # print ("\n\nfind_parameterized_file at " + path + " " + filename + "(" + fdir + ", " + fbase + ", " + fext + ") " + "  " + str(parameters))
+    if fdir == "":
+        # print ("    fdir is empty string")
+        fdir = path
+    else:
+        fdir = path + "/" + fdir
+    f = {}
+    for (dirpath, dirnames, filenames) in os.walk(fdir):
+        # f.extend(filenames)
+        # print ("dirpath: " + dirpath + ", dirnames: " + str(dirnames) + ", filenames: " + str(filenames))
+        for ffname in filenames:
+            if ffname.startswith(fbase + "."):
+                file_args = os.path.splitext(ffname)[0][len(fbase)+1:]
+                # print("... looking at  " + ffname + " in " + dirpath + " with arglist " + file_args)
+                argdict = arglist_to_paramdict(file_args)
+                match = True
+                for arg in argdict:
+                    if not arg in parameters or parameters[arg] != argdict[arg]:
+                        match = False
+                if match:
+                    f[ffname] = len(argdict)
+        break
+    # print ("choices are: " + str(f))
+    if len(f) == 0:
+        exit("Error:  File "+filename+" not found.  Callstack is: "+ str(filestack) + " Paramaters are: "+ str(parameters))
+        
+    maxargs = -1
+    chosenfile = ""
+    for name in f:
+        if f[name] > maxargs:
+            chosenfile = name
+            maxargs = f[name]
+    # print ("chose  " + fdir + "/" + chosenfile)
+    return fdir + "/" + chosenfile
 
-    if filename in filestack:
-        print("recursion cycle: "
-            + filename + " called when already in the stack: "
+
+def get_file_lines(pfilename, parsed_scripts, filestack):
+    if pfilename in filestack:
+        exit("Error:  Recursion cycle: "
+            + pfilename + " called when already in the stack: "
             + str(filestack))
-        return ["recursion cycle: "
-            + filename + " called when already in the stack: "
-            + str(filestack)]
 
-    file = open(filename, "r")
-    lines = file.readlines()
-    file.close()
+    lines = []
+    try:
+      file = open(pfilename, "r")
+      lines = file.readlines()
+      file.close()
+    except Exception as e:
+        exit("Error:  Error reading file " + pfilename + " -- " + str(e))
+
+    return lines
+
+
+def read_through_file(path, filename, parameters, parsed_scripts, filestack):
+    # print ("reading in " + path + " through " + filename + "(" + str(parameters) + ") from  " + str(filestack))
+    if os.path.split(filename)[0] != "":
+        path = path + "/" + os.path.split(filename)[0]
+        filename = os.path.split(filename)[1]
+    
+    pfilename = find_parameterized_file(path, filename, parameters, filestack)
+
+    if pfilename in parsed_scripts:
+        # print("found already parsed " + pfilename)
+        return parsed_scripts[pfilename]
+
+    # print("parmeterized filename is " + pfilename)
+    lines = get_file_lines(pfilename, parsed_scripts, filestack)
 
     steps = []
     step_num = 1
@@ -86,7 +157,7 @@ def read_through_file(filename, script, parsed_scripts, filestack):
     section = ""
     for line in lines:
         if line[0] == '#':
-            add_comment(script, step, section, line)
+            add_comment(step, section, line)
         elif line.strip() == '':
             steps = add_step(steps, step, section)
             if section != "" or "comment" in step:
@@ -100,8 +171,27 @@ def read_through_file(filename, script, parsed_scripts, filestack):
                 section = linematch.group(1).lower()
                 description = linematch.group(2)
             else:
-                description = "\n"+ line
-            if linematch and section == "call":
+                description = line
+            if linematch and section == "set":
+                steps = add_step(steps, step, last_section)
+                
+                if last_section != "" or "comment" in step:
+                    step_num += 1
+                    step = {"id": str(step_num), "order": step_num}
+                section = ""
+                
+                set_param_match = re.match("\s*(.+)\s*=\s*(.+)\s*", linematch.group(2))
+                parameter = set_param_match.group(1)
+                value = set_param_match.group(2)
+                step["set"] = {"parameter": parameter, "value": value}
+                if not parameter in parameters:
+                    parameters[parameter] = value
+                    # print ("added parameter: " + parameter + ", value: " + value)
+                steps = add_step(steps, step, section)
+                
+                step_num += 1
+                step = {"id": str(step_num), "order": step_num}                
+            elif linematch and section == "call":
                 steps = add_step(steps, step, last_section)
 
                 if last_section != "" or "comment" in step:
@@ -109,14 +199,17 @@ def read_through_file(filename, script, parsed_scripts, filestack):
                     step = {"id": str(step_num), "order": step_num}
                 section = ""
                 step["call"] = linematch.group(2)
+                # print("call found  " + step["call"])
                 call_file_match = re.match("(.+)\((.*)\)\s*", linematch.group(2))
                 call_file = call_file_match.group(1) + ".tl"
                 step["name"] = call_file
+                call_parameters = {**parameters, **arglist_to_paramdict(call_file_match.group(2))}
+                filestack.append(pfilename)
+                # print("... setting step filename ")
+                step["filename"] = find_parameterized_file(path, call_file, call_parameters, filestack)
                 step["parameters"] = arglist_to_paramdict(call_file_match.group(2))
-                filestack.append(filename)
-                if os.path.dirname(filename) != '':
-                    call_file = os.path.dirname(filename) + "/" + call_file
-                step["steps"] = read_through_file(call_file, step,
+                # print("call step added " + pfilename + " to " + str(filestack))
+                step["steps"] = read_through_file(path, call_file, call_parameters, 
                         parsed_scripts, filestack)
                 filestack.pop()
                 steps = add_step(steps, step, section)
@@ -126,41 +219,50 @@ def read_through_file(filename, script, parsed_scripts, filestack):
             else:
                 add_original_description(step, section, description)
     steps = add_step(steps, step, section)
-    parsed_scripts[filename] = steps
+    parsed_scripts[pfilename] = steps
     return steps
 
 
 def parse_arguments():
     argparser = argparse.ArgumentParser(description="taligen: generate json file from tl file")
     argparser.add_argument("tl_file", type=str, help=".tl (task list) file to generate from")
-    argparser.add_argument("substitutions", type=str, nargs="*", help="substitutions to make in the file")
+    argparser.add_argument("parameters", type=str, nargs="*", help="parameters to substitute for variables in the .tl files")
     return argparser.parse_args()
 
 
 def collect_pass(args):
     script = {"name": args.tl_file}
     script["generated"] = str(datetime.datetime.now())
-    script["parameters"] = arglist_to_paramdict(args.substitutions)
-    script["steps"] = read_through_file(args.tl_file, script, {}, deque())
+    script["parameters"] = arglist_to_paramdict(args.parameters)
+    script["steps"] = read_through_file(".", args.tl_file, script["parameters"], {}, deque())
     return script
 
 
-def replace_within_description(step, part, parameters):
-    if part in step and len(parameters) > 0:
-        for key, value in parameters.iteritems():
+def replace_within_description(step, part, parameters, filestack):
+    if isinstance(step, dict) and part in step and len(parameters) > 0:
+        # for key, value in parameters.iteritems():
+        for key, value in parameters.items():
             step[part]["description"] = re.sub("\\$"+key, value, step[part]["description"])
+        set_vars = re.findall(r"\$\w+", step[part]["description"])
+        for var in set_vars:
+            # print("Missing " + var[1:] + " for " + step[part]["description"])
+            # step["comment"] = step.get("comment", "") + "\nMissing " + var[1:] + " for " + step[part]["description"]
+            # return add_error(step, part, "Missing " + var[1:] + " for " + step[part]["description"])
+            exit("Error: Missing value for variable " + var[1:] + " for step " + step["id"] + " " + part + " " + step[part]["description"] + " call stack is " + str(filestack))
     return step
 
 
-def replace_pass(script, parameters):
+def replace_pass(script, parameters, filestack):
     myparameters = parameters.copy()
     myparameters.update(script["parameters"])
     if len(myparameters) > 0:
         for step in script["steps"]:
-            step = replace_within_description(step, "a", myparameters)
-            step = replace_within_description(step, "o", myparameters)
+            step = replace_within_description(step, "a", myparameters, filestack)
+            step = replace_within_description(step, "o", myparameters, filestack)
             if "steps" in step:
-                step = replace_pass(step, myparameters)
+                filestack.append(step["filename"]+"("+str(step["parameters"])+")")
+                step = replace_pass(step, myparameters, filestack)
+                filestack.pop()
     return script
 
 
@@ -168,11 +270,11 @@ def main():
     args = parse_arguments()
 
     dt = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    json_filename = dt+"."+os.path.splitext(args.tl_file)[0]+params_to_string(args.substitutions)+'.json'
+    json_filename = dt+"."+os.path.splitext(args.tl_file)[0]+params_to_string(args.parameters)+'.json'
 
     script = collect_pass(args)
     script["filename"] = json_filename
-    script = replace_pass(script, {})
+    script = replace_pass(script, {}, [args.tl_file+"("+str(args.parameters)+")"])
 
     with open(json_filename, 'w') as fp:
         json.dump(script, fp, indent=4)
